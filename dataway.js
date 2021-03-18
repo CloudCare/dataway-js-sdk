889c1d21d0eb77bdac88070c9fae1b17a00434cd
Yiling Zhou <pastgift@gmail.com>
2021-03-18 23:47:04 +0800

    bug fix

--- dataway.js
+++ dataway.js
@@ -1988,7 +1988,12 @@
 
     opt.method  = 'GET';
     opt.path    = opt.path    || this.path;
+    opt.query   = opt.query   || {};
     opt.headers = opt.headers || {};
+
+    if (this.token) {
+      opt.query['token'] = this.token;
+    }
 
     var _authHeaders = this._prepareAuthHeaders(opt);
     Object.assign(opt.headers, _authHeaders);
